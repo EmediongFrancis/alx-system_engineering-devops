da033023e83aae647dc3d6d0fbc1ed6e231279ea
Emediong Francis <emediongfrancis@gmail.com>
2022-01-31 19:31:52 +0100

    Test code

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,40 +4,30 @@
     Gathers data from an API.
 '''
 
-if __name__ == "__main__":
-    from requests import get
-    from sys import argv, exit
+import re
+import requests
+import sys
 
-    try:
-        id = argv[1]
-        is_int = int(id)
-    except Exception:
-        exit()
 
-    url_user = "https://jsonplaceholder.typicode.com/users?id=" + id
-    url_todo = "https://jsonplaceholder.typicode.com/todos?userId=" + id
+API_URL = 'https://jsonplaceholder.typicode.com'
+'''The API's URL.'''
 
-    r_user = get(url_user)
-    r_todo = get(url_todo)
 
-    try:
-        js_user = r_user.json()
-        js_todo = r_todo.json()
-
-    except ValueError:
-        print("Not a valid JSON")
-
-    if js_user and js_todo:
-        EMPLOYEE_NAME = js_user[0].get('name')
-        TOTAL_NUMBER_OF_TASKS = len(js_todo)
-        NUMBER_OF_DONE_TASKS = sum(item.get("completed")
-                                   for item in js_todo if item)
-
-        print("Employee {} is done with tasks({}/{}):"
-              .format(EMPLOYEE_NAME,
-                      NUMBER_OF_DONE_TASKS,
-                      TOTAL_NUMBER_OF_TASKS))
-        for todo in js_todo:
-            TASK_TITLE = todo.get('title')
-            if todo.get("completed"):
-                print("\t {}".format(TASK_TITLE))
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        if re.fullmatch(r'\d+', sys.argv[1]):
+            id = int(sys.argv[1])
+            user_res = requests.get('{}/users/{}'.format(API_URL, id)).json()
+            todos_res = requests.get('{}/todos'.format(API_URL)).json()
+            user_name = user_res.get('name')
+            todos = list(filter(lambda x: x.get('userId') == id, todos_res))
+            todos_done = list(filter(lambda x: x.get('completed'), todos))
+            print(
+                'Employee {} is done with tasks({}/{}):'.format(
+                    user_name,
+                    len(todos_done),
+                    len(todos)
+                )
+            )
+            for todo_done in todos_done:
+                print('\t {}'.format(todo_done.get('title')))
