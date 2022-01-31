5fd587267639f70844a4d91f0b273154742f3e1a
Emediong Francis <emediongfrancis@gmail.com>
2022-01-31 16:21:27 +0100

    Fetch employee data w/API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -14,22 +14,22 @@ def getData():
     '''
     ListOfUsers = req.get('https://jsonplaceholder.typicode.com/users').json()
     ListOfTodos = req.get('https://jsonplaceholder.typicode.com/todos').json()
-    TotalTasks = 0
-    TotalCompletedTasks = 0
+    TOTAL_NUMBER_OF_TASKS = 0
+    NUMBER_OF_DONE_TASKS = 0
     TaskDescription = []
     for user in ListOfUsers:
         if user.get('id') == int(arg[1]):
-            Employee = user.get('name')
+            EMPLOYEE_NAME = user.get('name')
             break
     for todo in ListOfTodos:
         if todo.get('userId') == int(arg[1]):
-            TotalTasks += 1
+            TOTAL_NUMBER_OF_TASKS += 1
             if todo.get('completed') is True:
-                TotalCompletedTasks += 1
+                NUMBER_OF_DONE_TASKS += 1
                 TaskDescription.append(todo.get('title'))
-    print('Employee {} is done with tasks({}/{}):'.format(Employee,
-                                                          TotalCompletedTasks,
-                                                          TotalTasks))
+    print('Employee {} is done with tasks({}/{}):'.format(EMPLOYEE_NAME,
+                                                          NUMBER_OF_DONE_TASKS,
+                                                          TOTAL_NUMBER_OF_TASKS))
 
     for task in TaskDescription:
         print('\t {}'.format(task))
