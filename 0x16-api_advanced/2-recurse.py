9e9e9c7d6d63fe8153f3c42e5caadb06a5983ac5
Emediong Francis <emediongfrancis@gmail.com>
2022-02-16 20:46:33 +0100

    Print total number of titles for given subreddit

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -5,25 +5,49 @@
 '''
 
 
-def recurse(subreddit, hot_list=[]):
-    '''
-        Returns a list containing the titles of all hot
-        articles for a given subreddit.
-    '''
-    import requests
+from requests import get
 
-    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0'}
+REDDIT = "https://www.reddit.com/"
+HEADERS = {'user-agent': 'my-app/0.0.1'}
+
+
+def recurse(subreddit, hot_list=[], after=""):
+    '''
+        Returns a list containing the titles of all hot articles for a given
+        subreddit. If no results are found for the given subreddit,
+        the function should return None.
+    '''
+    if after is None:
+        return hot_list
+
+    url = REDDIT + "r/{}/hot/.json".format(subreddit)
+
+    params = {
+        'limit': 100,
+        'after': after
+    }
+
+    r = get(url, headers=HEADERS, params=params, allow_redirects=False)
+
+    if r.status_code != 200:
+        return None
+
     try:
-        req = requests.get(url, headers=headers, allow_redirects=False)
-        if req.status_code == 200:
-            req = req.json()
-            for post in req['data']['children']:
-                hot_list.append(post['data']['title'])
-            if len(req['data']['children']) > 0:
-                recurse(subreddit, hot_list)
-            return hot_list
-        else:
-            return None
+        js = r.json()
+
+    except ValueError:
+        return None
+
+    try:
+
+        data = js.get("data")
+        after = data.get("after")
+        children = data.get("children")
+        for child in children:
+            post = child.get("data")
+            hot_list.append(post.get("title"))
+
     except Exception:
         return None
+
+    return recurse(subreddit, hot_list, after)
